e69e955e347322e1466ef49d6764582eb8f1fc28
JuanSuppicich <suppicichjuan@gmail.com>
2021-04-07 15:19:04 -0300

    Add Spring Initializr

---
+++ src/main/java/com/durandsuppicich/danmspagos/DanMsPagosApplication.java
@@ -0,0 +1,13 @@
+package com.durandsuppicich.danmspagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsPagosApplication.class, args);
+	}
+
+}
