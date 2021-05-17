05adb3741a6fa8846afca7ee9a93121a01ccb5a5
Matias Durand <matiasjdurand@gmail.com>
2021-05-16 21:03:46 -0300

    Add logger

--- src/main/java/com/durandsuppicich/danmspagos/DanMsPagosApplication.java
+++ src/main/java/com/durandsuppicich/danmspagos/DanMsPagosApplication.java
@@ -2,8 +2,10 @@ package com.durandsuppicich.danmspagos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class DanMsPagosApplication {
 
 	public static void main(String[] args) {
