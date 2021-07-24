f424d18b21579119ecb1be812caaeff09a14057e
Matias Durand <matiasjdurand@gmail.com>
2021-07-24 17:37:54 -0300

    Register service with eureka

--- src/main/java/com/durandsuppicich/danmspagos/DanMsPagosApplication.java
+++ src/main/java/com/durandsuppicich/danmspagos/DanMsPagosApplication.java
@@ -2,10 +2,12 @@ package com.durandsuppicich.danmspagos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableDiscoveryClient
 public class DanMsPagosApplication {
 
 	public static void main(String[] args) {
